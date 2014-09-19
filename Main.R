d7874091fe62dee0739f91afad212424bfb705c3
mnavascues <navascues@supagro.inra.fr>
2014-09-19 15:09:12 +0200

    first

---
+++ Main.R
@@ -0,0 +1,17 @@
+##################################################################################
+#
+# Script to test the Fc outlier method modified from Goldringer and Bataillon 2002
+# 
+##################################################################################
+
+# This scripts uses SLiM (Messer 2013) for simulating the data
+# http://messerlab.org/software/
+
+
+dcad
+
+
+
+
+
+
