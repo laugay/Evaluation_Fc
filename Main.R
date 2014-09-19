7cad94b157fb8603ba3436ac5efde9e1681d740f
mnavascues <navascues@supagro.inra.fr>
2014-09-19 16:22:31 +0200

    Adding parameters

--- Main.R
+++ Main.R
@@ -7,11 +7,32 @@
 # This scripts uses SLiM (Messer 2013) for simulating the data
 # http://messerlab.org/software/
 
+# set working directory
+setwd("/home/miguel/Work/Research/2012.SelfAdapt/Evaluation_Fc")
 
-dcad
+# high penality for scientific notation (necessary to input large number in SLiM)
+options("scipen"=999)
 
+# tools to write/read SLiM input/output
+source("slim_tools.R")
 
+#------------
+# PARAMETERS
+#------------
 
+# selfing rate
+sigma <- 0.9 
+# mutation rate per bp
+u <- 2e-6
+# genome size
+genome_length <- 25000
+# theta= 4Neu (for the genome)
+theta <- 50
+N <- theta/(4*u*genome_length)
+# selection coeficient
+new_scoef <- 0.6
 
+set.seed(368898)
 
+#
 
