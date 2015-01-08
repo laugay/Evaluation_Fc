19c34e827d63ef2d68faab8fcebfd16c736d9c48
mnavascues <navascues@supagro.inra.fr>
2015-01-08 16:41:08 +0100

    some minor additions, changes in file naming, recombination rate with several chromosomes...

--- Main.R
+++ Main.R
@@ -46,8 +46,12 @@ simID <- "test"
 sigma <- 0.9 
 # mutation rate per bp
 u <- 2e-6
+# recombination rate per bp
+r <- 2e-6
 # genome size
 genome_length <- 25000
+# number of chromosomes
+chr_num <- 8
 # theta= 4Neu (for the genome)
 theta <- 50
 # selection coeficient
@@ -70,7 +74,8 @@ parseCommandArgs()
 N <- theta/(4*u*genome_length)
 # length of pure drift period
 drift_period_duration <- number_of_times*N
-
+# Simulation scenario ID for file identification
+simID <- paste0("scenario_",simID)
 
 
 
@@ -86,20 +91,23 @@ slim_out_drift <- paste0(simID,"_drift.out")
 slim_log_drift <- paste0(simID,"_drift.log")
 
 # Write slim input file
-writeMutation      (file=slim_in_drift, number_of_types=1, h=0.5, DFE="f", s=0)
-writeMutationRate  (file=slim_in_drift, u=u)  
-writeGenomicElement(file=slim_in_drift, number_of_types=1, mut_type=list("m1"), prop=list(1))
-writeChromosome    (file=slim_in_drift, element_type="g1", start=1, end=genome_length)
-writeRecombination (file=slim_in_drift, interval_end=genome_length, r=0)
-writeGenerations   (file=slim_in_drift, t=drift_period_duration, append=T)
-writeDemography    (file=slim_in_drift, type="P", time=1, pop="p1", N=N)
-writeDemography    (file=slim_in_drift, type="S", time=1, pop="p1", sigma=sigma, append_demography=T)
-writeOutput        (file=slim_in_drift, type="A", time=drift_period_duration, filename=slim_out_drift)
-writeSeed          (file=slim_in_drift, seed=round(runif(1,-2^31,2^31)) )
+writeMutation          (file=slim_in_drift, number_of_types=1, h=0.5, DFE="f", s=0)
+writeMutationRate      (file=slim_in_drift, u=u)  
+writeGenomicElement    (file=slim_in_drift, number_of_types=1, mut_type=list("m1"), prop=list(1))
+writeChromosome        (file=slim_in_drift, element_type="g1", start=1, end=genome_length)
+writeRecombinationChrom(file=slim_in_drift, chr_num=chr_num, genome_length=genome_length, r=r, append=T)
+writeGenerations       (file=slim_in_drift, t=drift_period_duration, append=T)
+writeDemography        (file=slim_in_drift, type="P", time=1, pop="p1", N=N)
+writeDemography        (file=slim_in_drift, type="S", time=1, pop="p1", sigma=sigma, append_demography=T)
+writeOutput            (file=slim_in_drift, type="A", time=drift_period_duration, filename=slim_out_drift)
+writeSeed              (file=slim_in_drift, seed=round(runif(1,-2^31,2^31)) )
 
 # Run slim
 system(paste("./slim",slim_in_drift,">",slim_log_drift))
 # NB: slim executable must be in the same folder as slim input file
+
+# message(paste(Sys.time(),"Episode de dérive slim terminé, répétition n° ",sim))
+
 
 # 2. SIMULATION OF THE PERIOD WITH SELECTION (in between samples)
 
@@ -108,13 +116,21 @@ slim_in_selection   <- paste0(simID,"_selection.txt")
 slim_out_selection  <- paste0(simID,"_selection.out")
 slim_log_selection  <- paste0(simID,"_selection.log")
 slim_init_selection <- paste0(simID,"_selection_init.txt")
+selestim_in         <- paste0(simID,"_selestim_in")
+selestim_out        <- paste0(simID,"_selestim_out")
+mutable_name        <- paste0(simID,"_muttable.RData") 
+Fstat_name          <- paste0(simID,"_Fstat.RData")
+sampled_ind_name    <- paste0(simID,"_sampled_ind.RData")
+
+
+
 
 # Read slim output from DRIFT period
-out_drift_lines <- readLines(con=slim_out_drift)
-pop_line        <- which(out_drift_lines=="Populations:")
-mut_line        <- which(out_drift_lines=="Mutations:")
-gen_line        <- which(out_drift_lines=="Genomes:")
-num_of_mut      <- gen_line-mut_line-1
+out_drift_lines      <- readLines(con=slim_out_drift)
+out_drift_pop_line   <- which(out_drift_lines=="Populations:")
+out_drift_mut_line   <- which(out_drift_lines=="Mutations:")
+out_drift_gen_line   <- which(out_drift_lines=="Genomes:")
+out_drift_num_of_mut <- gen_line-mut_line-1
 
 if (mode=="new mutation")       cat("Adaptation though new mutation not implemented yet, using standing variation instead")
 if (mode!="standing variation") cat("Adaptation mode undefined, using standing variation") 
@@ -122,32 +138,59 @@ if (mode!="standing variation") cat("Adaptation mode undefined, using standing v
 # SELECTION ON STANDING VARIATION (changes selection coefficient of on a random locus)
 
 # chose a random locus
-sampled_mut                   <- sample(x=num_of_mut, size=1)
+sampled_mut                   <- sample(x=out_drift_num_of_mut, size=1)
 # change selection coefficient of locus (derived allele being advantageous) # TODO: choose randomly the advanageuous allele
-mutation_table                <- read.table(file=slim_out_drift, skip=mut_line, nrows=num_of_mut)
+mutation_table                <- read.table(file=slim_out_drift, skip=out_drift_mut_line, nrows=out_drift_num_of_mut)
 levels(mutation_table[,2])    <- c("m1","m2") 
 mutation_table[sampled_mut,2] <- "m2"
 mutation_table[sampled_mut,4] <- sel_coef
 mutation_table[sampled_mut,5] <- dominance_coef
 
 # write initialzation file for slim (state of populaion at starting point of selection period)
-write(out_drift_lines[pop_line:mut_line], slim_init_selection)
+write(out_drift_lines[out_drift_pop_line:out_drift_mut_line], slim_init_selection)
 write.table(mutation_table, slim_init_selection, append=T, quote=F, col.names=F)
 write(out_drift_lines[-(1:(gen_line-1))], slim_init_selection, append=T)
 
+# message(paste(Sys.time(),"Episode de SV slim commence, répétition n° ",sim))
+
 # write input file for slim
-writeMutation      (file=slim_in_selection, number_of_types=2, h=0.5, DFE="f", s=c(0,sel_coef), append=F, append_mutation=F)
-writeMutationRate  (file=slim_in_selection, u=u)  
-writeGenomicElement(file=slim_in_selection, number_of_types=1, mut_type=list("m1","m2"), prop=list(1,0))
-writeChromosome    (file=slim_in_selection, element_type="g1", start=1, end=genome_length)
-writeRecombination (file=slim_in_selection, interval_end=genome_length, r=0)
-writeGenerations   (file=slim_in_selection, t=selection_period_duration, append=T)
-writeOutput        (file=slim_in_selection, type="A", time=selection_period_duration, filename=slim_out_selection)
-writeSeed          (file=slim_in_selection, seed=round(runif(1,-2^31,2^31)) )
+writeMutation          (file=slim_in_selection, number_of_types=2, h=0.5, DFE="f", s=c(0,sel_coef), append=F, append_mutation=F)
+writeMutationRate      (file=slim_in_selection, u=u)  
+writeGenomicElement    (file=slim_in_selection, number_of_types=1, mut_type=list("m1","m2"), prop=list(1,0))
+writeChromosome        (file=slim_in_selection, element_type="g1", start=1, end=genome_length)
+writeRecombinationChrom(file=slim_file_2, chr_num=chr_num, genome_length=genome_length, r=r, append=T)  
+writeGenerations       (file=slim_in_selection, t=selection_period_duration, append=T)
+writeOutput            (file=slim_in_selection, type="A", time=selection_period_duration, filename=slim_out_selection)
+writeSeed              (file=slim_in_selection, seed=round(runif(1,-2^31,2^31)) )
 
 write("#INITIALIZATION",file=slim_in_selection,ncolumns=1,append=TRUE)
 write(slim_init_selection,file=slim_in_selection,ncolumns=1,append=TRUE)
 
 system(paste("./slim",slim_in_selection,">",slim_log_selection))
+
+# message(paste(Sys.time(),"Episode de SV slim terminé, répétition n° ",sim))
+
+# Read slim output and log from SELECTION period
+out_selection_lines    <- readLines(con=slim_out_selection)
+out_selection_mut_line <- which(out_selection_lines=="Mutations:")
+out_selection_gen_line <- which(out_selection_lines=="Genomes:")
+
+log_selection_lines    <- readLines(con=slim_log_selection)
+log_selection_mut_line <- which(log_selection_lines=="Mutations:")
+log_selection_num_mut  <- length(log_selection_lines)-log_selection_mut_line
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
